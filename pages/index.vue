5a0a707110cbf04dc01a2b336a7a57a5c5517309
zero3william <zero3william@gmail.com>
2019-07-11 00:44:28 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/feed">to post preview</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+</style>
