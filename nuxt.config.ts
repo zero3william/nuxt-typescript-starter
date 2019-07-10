85d3c8e923c67d3fb93c2d136df323b62adc3923
zero3william <zero3william@gmail.com>
2019-07-11 01:29:13 +0800

    add tslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,25 +7,21 @@ const config: NuxtConfiguration = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   loading: { color: '#fff' },
-  css: [
-  ],
-  plugins: [
-  ],
-  modules: [
-    '@nuxtjs/axios',
-  ],
-  axios: {
-  },
+  css: [],
+  plugins: [],
+  modules: ['@nuxtjs/axios'],
+  axios: {},
   build: {
-    extend(config, ctx) {
-    }
+    extend(config, ctx) {}
   }
 }
 
